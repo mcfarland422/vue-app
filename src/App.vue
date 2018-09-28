21bca294ed77dd25ca839c8fcfff17819918a602
Michael McFarland <mcfarland422@mcfarlands-mbp.attlocal.net>
2018-09-28 14:10:47 -0400

    second

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+   <test></test>
   </div>
 </template>
 
 <script>
+import Test from "./components/Test";
+
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Test
   }
 };
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="test">
+    <input type="text" v-model="title"><br />
+    <h1>{{title}}</h1>
+    <p v-if="showName">{{user.firstName}}</p>
+    <p v-else="showName">Nobody</p>
+    <ul>
+      <li v-for="item in items">{{item.title}}</li>
+    </ul>
+    <button v-on:click="greet('Hello World')">Say Greeting</button>
+    <br />
+    <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  data() {
+    return {
+      title: "Hello World",
+      user: {
+        firstName: "John",
+        lastName: "Doe"
+      },
+      showName: true,
+      items: [
+        { title: "Item One" },
+        { title: "Item Two" },
+        { title: "Item Three" }
+      ]
+    };
+  },
+  methods: {
+    greet: function(greeting) {
+      alert(greeting);
+    },
+    pressKey: function(e) {
+      console.log(e.target.value);
+    },
+    enterHit: function() {
+      console.log("You hit enter");
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
